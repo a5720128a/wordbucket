1e64b96e686fb75e2180cf45fbc58c889e8e5aa1
suppakan <suppakan@ubuntu.ubuntu-domain>
2018-02-26 00:32:58 +0700

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -16,10 +16,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention Word Bucket lists
         self.assertIn('Word Bucket', self.browser.title)
-        self.fail('Finish the test!')  
-
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('Word Bucket', header_text) 
+        
         # She is invited to enter a word item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Add new word'
+        )
+        
         # She types "weeb" into a text box
+        inputbox.send_keys('weeb')
+        
+        # When she hits enter, the page updates, and now the page word lists
+        # "1: weeb" as an item in a word list table
+        inputbox.send_keys(Keys.ENTER)  
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')  
+        self.assertTrue(
+            any(row.text == '1: weeb' for row in rows)
+        )
+        self.fail('Finish the test!') 
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')
