85b5d41f3d0243fc8383e7df712eab918b94af15
suppakan <suppakan@ubuntu.ubuntu-domain>
2018-02-26 00:18:17 +0700

    First webpage's title

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):  
 
-assert 'Django' in browser.title
+    def setUp(self):  
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        # Ann has heard about a cool new online word app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention Word Bucket lists
+        self.assertIn('Word Bucket', self.browser.title)
+        self.fail('Finish the test!')  
+
+        # She is invited to enter a word item straight away
+        # She types "weeb" into a text box
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')
